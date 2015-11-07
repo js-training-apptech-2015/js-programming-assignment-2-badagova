31976b7d72ae2f99c3ec69bd12b607e5c19d3a23
badagova <jenny_bad@mail.ru>
2015-11-07 11:00:50 +0500

    Task 1,2,3

--- JS-2.js
+++ JS-2.js
@@ -20,20 +20,13 @@ function solution12(a){ // Task #1 without .indexOf
 }
 
 function solution2(s){
-  var bracketStack = [],
-      temp = '';
+  var bracketStack = [];
   for (var i = 0; i < s.length; i++) {
     switch (s[i]) {
-      case '[': {
-        bracketStack.push('[');
-        break;
-      }
-      case '{': {
-        bracketStack.push('{');
-        break;
-      }
+      case '[': 
+      case '{': 
       case '(': {
-        bracketStack.push('(');
+        bracketStack.push(s[i]);
         break;
       }
       case ']': {
@@ -61,7 +54,30 @@ function solution2(s){
   }
   return true;
 }
+
+function solution3(a, f){
+  var arg, 
+      func, 
+      functionToApply,
+      functionBody, 
+      result = [],
+      patternArg = /(\(?)(\w*)(\)?)(?=\=>)/,
+      patternExp = /(\=>)(.+$)/;
+  arg = patternArg.exec(f);
+  func = patternExp.exec(f);
+  functionBody = 'return ' + func[2];
+  if (arg[2] == '') {
+    functionToApply = new Function(functionBody);  
+  } else {
+    var functionToApply = new Function(arg[2], functionBody);
+  }
+  for (var i = 0; i < a.length; i++){
+    result[i] = functionToApply(a[i]);
+  }
+  return result;
+}
 var mas = [1,0,4.6,6,2,3,-7,9,8,5,11];
-/*alert(solution11(mas));
-alert(solution12(mas));*/
-alert(solution2('[(])'));
+alert(solution11(mas));
+alert(solution12(mas));
+alert(solution2('({){'));
+alert(solution3([1,2],'(a)=>a*2'));
